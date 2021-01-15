f4f73e0fe4cff7fab09d4968fe1ec62b0dc20176
luan nascimento <almeida.luan099@gmail.com>
2021-01-15 18:34:59 -0300

    Calculando o dano de atk e aplicando na vida do player

--- ex_mon/lib/game/Attack.ex
+++ ex_mon/lib/game/Attack.ex
@@ -1,15 +1,30 @@
 defmodule ExMon.Game.Attack do
+  alias ExMon.Game
 
   @move_rnd_power 18..25
   @move_avg_power 10..20
 
   def attack(move, opponent) do
-    move
-    |> calculate_power()
+    damage = calculate_power(move)
+
+    opponent
+    |> Game.fetch_player()
+    |> Map.get(:life)
+    |> calculate_total_life(damage)
+    |> update_life_opponent(opponent)
 
   end
 
   def calculate_power(:move_avg), do: Enum.random(@move_avg_power)
   def calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
 
+
+  def calculate_total_life(life, damage) when life  - damage < 0, do: 0
+  def calculate_total_life(life, damage), do: life - damage
+
+  def update_life_opponent(life,opponent) do
+    opponent
+    |> Game.fetch_player()
+    |> Map.put(:life, life)
+  end
 end
