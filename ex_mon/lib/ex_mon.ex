c119cfcaaca64a58f79e3b10010efaca461fa486
luan nascimento <almeida.luan099@gmail.com>
2021-01-15 15:17:43 -0300

    Gerenciando estado com Agent default do Elixir,Criando modulo Game que cria automaticamente o player computer

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,6 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{ Player, Game}
+  alias ExMon.Game.Status
+  @computer_name "RoboTinik"
+
   def create_player(name, move_rnd, mov_avg, move_heal) do
     Player.build(name, move_rnd, mov_avg, move_heal)
+  end
+
+  def create_game(player) do
+    @computer_name
+    |> create_player(:kick,:punch,:heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
+
   end
 end
